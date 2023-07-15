bfc1838fc485a3da32d617395f12e7ea17008243
brianbryank <brianbryan0125@gmail.com>
2023-07-15 18:47:09 +0300

    complete assigment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, attribute) {
+    return drivers.filter(function (driver) { return driver.toLowerCase() === attribute.toLowerCase() })
+  }
+  //This function takes an array of drivers and a string as arguments. 
+  //In this function, each element of the drivers array is a JavaScript object that has a property of name.
+  // The function should return each element whose name property matches the provided string argument.
+  
+  function fuzzyMatch(drivers, attribute) {
+    return drivers.filter(function (driver) { return driver.toLowerCase().substring(0, attribute.length) === attribute.toLowerCase() })
+  }
+  
+  function matchName(drivers, argument) {
+    return drivers.filter(function (driver) { return driver.name === argument })
+  }
